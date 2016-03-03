91d68106098b599dccb98ad0aea8045195960432
LiuDD <498854744@qq.com>
2016-03-03 18:41:13 +0800

    first commit

---
+++ app/src/test/java/examination_oa/com/examination_oa/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package examination_oa.com.examination_oa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
